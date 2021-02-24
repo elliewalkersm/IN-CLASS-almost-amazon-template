925f319d4720287bb97d886a98369100f87b7b25
elliewalkersm <elliewalkersm@gmail.com>
2021-02-23 21:41:31 -0600

    working on authors

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -1,12 +1,22 @@
 // FIXME: STUDENTS show your authors
 
 const showAuthors = (array) => {
+  document.querySelector('#add-button').innerHTML = '<button class="btn btn-success btn-lg mb-4" id="add-book-btn">Add An Author</button>';
   document.querySelector('#store').innerHTML = '';
   // CREATE A BUTTON TO ADD BOOKS
 
+
   array.forEach((item) => {
     // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `<div class="card">
+    <div class="card-body" style="height: 180px;">
+    <h5 class="card-title">${item.email}</h5>
+    <h5 class="card-title">${item.first_name}</h5>
+    <h5 class="card-title">${item.last_name}</h5>
+    <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="edit-author-btn--${item.firebaseKey}">Edit Author</button>
+    <button class="btn btn-danger" id="delete-author--${item.firebaseKey}">Delete Author</button>
+    </div>
+  </div>`;
   });
 };
 
--- src/javascripts/helpers/data/bookData.js
+++ src/javascripts/helpers/data/bookData.js
@@ -1,7 +1,19 @@
-// API CALLS FOR BOOKS
+// API CALLS
+import axios from 'axios';
+import firebaseConfig from '../auth/apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
 
 // GET BOOKS
+const getBooks = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/books.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
 // DELETE BOOK
 // CREATE BOOK
 // UPDATE BOOK
 // SEARCH BOOKS
+
+export default getBooks;
